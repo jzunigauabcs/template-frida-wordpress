1894038cd3b37e8fa5e4e24d38bd89cf6ddaa6a9
AlexisICE <alexis_ivan_cota@live.com.mx>
2023-10-10 09:01:24 -0700

    Se agrego hook para poder remover el boton de ordenar otra vez en el checkout

--- functions.php
+++ functions.php
@@ -824,6 +824,8 @@ remove_action ('woocommerce_single_product_summary', 'woocommerce_template_singl
 
 remove_action ('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 
+remove_action( 'woocommerce_order_details_after_order_table', 'woocommerce_order_again_button' );
+
 // add_filter('gettext',  'translate_text');
 // add_filter('ngettext',  'translate_text');
  
